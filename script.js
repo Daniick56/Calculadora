38a7290e5030692a0c7e5abd48e6aa96fa23083f
Daniick56 <danielballesteros22@outlook.com>
2024-02-22 19:08:41 -0500

    Reorganizando el codigo para mayor comodidad en la lectura.

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ function sumar(num1, num2) {
 
 // Función principal del programa
 function calculadora() {
-  var opcion = prompt(
-    "Elija una opción:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Factorial\n6. Exponente\n7. Raíz cuadrada"
-  );
+  var opcion = prompt("Elija una opción:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Factorial\n6. Exponente\n7. Raíz cuadrada");
   var num1 = parseFloat(prompt("Ingrese el primer número:"));
-  var num2, resultado;
+  var num2;
+  var resultado;
 
   if (opcion !== "5" && opcion !== "7") {
     num2 = parseFloat(prompt("Ingrese el segundo número:"));
